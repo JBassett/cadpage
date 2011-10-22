4468f944461d761dee7bcfca9b674aca692f616a
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-10-22 21:14:20 +0000

    v1.4.32 release

---
+++ tags/1.4.32/cadpage/src/net/anei/cadpage/donation/PaidDonateEvent.java
@@ -0,0 +1,46 @@
+package net.anei.cadpage.donation;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import net.anei.cadpage.R;
+
+/**
+Your Cadpage subscription is current
+
+Your current Cadpage subscription will expire on\n%s
+ */
+public class PaidDonateEvent extends DonateScreenEvent {
+  
+  public PaidDonateEvent() {
+    super(AlertStatus.GREEN, R.string.donate_paid_title, R.string.donate_paid_text,
+          AndroidDonateEvent.instance());
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return (DonationManager.status() == DonationManager.DonationStatus.PAID);
+  }
+
+  @Override
+  protected Object[] getTextParms(int type) {
+    
+    switch (type) {
+      
+    case PARM_TEXT:
+      Date expireDate = DonationManager.expireDate();
+      String sDate = DateFormat.getDateInstance(DateFormat.MEDIUM).format(expireDate);
+      return new Object[]{sDate};
+
+    default:
+      return null;
+    }
+  }
+  
+  private static final PaidDonateEvent instance = new PaidDonateEvent();
+  
+  public static PaidDonateEvent instance() {
+    return instance;
+  }
+
+}
---
+++ tags/1.4.32/cadpage/src/net/anei/cadpage/parsers/CO/COGreeleyParser.java
@@ -0,0 +1,70 @@
+package net.anei.cadpage.parsers.CO;
+
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.SmsMsgInfo.Data;
+import net.anei.cadpage.parsers.FieldProgramParser;
+
+/*
+20442,ALARMF,23691 CR 60H.E1 E4 L1,TEXT:AUDIBLE FROM GENERAL AND SMOKE DETECTOR \COMP:1ST CLASS SECURITY \PH:800 482 9800,
+20431,SMKODR,1ST ST/8TH AV.E4 L1,TEXT:SMELL OF NATURAL GAS ALL OVER THE AREA \COMP:W012,
+20373,SMKODR,10TH AV/22ND ST.E4 R1,TEXT:STRONG SMELL OF NATURAL GAS IN AIR IN THIS AREA \COMP:MIKE RUANE \PH:608 630 2959,
+20032,SMKODR,2114 BLUEBELL AV.E4 R1,TEXT:FROM THE SOUTH, STRONG SMELL OF NATURAL GAS UNK WHERE ITS COMING FROM,
+19897,ALARMF,2013 1ST AV.E4 E1 L1,TEXT:AUDIBLE COMING FROM PULL STATION  NW DOOR, NO ATC, PREM PH 970 674 1957 \COMP:OP 311 SHERLOCK PLUS \PH:800 443
+19547,AIRAF ,600 AIRPORT RD.E4,TEXT:PLANE COMING IN WITHOUT LANDING GEAR.  RP WANTS FIRE AND MEDICAL TO RESPOND AND STANDBY. \COMP:MANDY \PH:336-3000,
+22668,TAUF  ,11TH AV/18TH ST.E2 R1 WA,TEXT:RP HIT A GUY IN CROSSWALK,HE\nIS TRYING TO WALK AWAY,
+
+Contact: Doug Gilliland <dgilliland88@gmail.com>
+Sender: CommtechMessenger@mauser.greeleypd.com
+  / 10903,SI    -SICK & INJ (F&A),3103 23RD AV.E2 WA,,\n\n\n
+  / 11470,SI    -SICK & INJ (F&A),3136 51ST AV.E5 WA,TEXT:RIB PAIN,FELL A WEEK \nAGO \COMP:DEL \PH:314-226-9976,\n\n\n
+
+ */
+
+
+public class COGreeleyParser extends FieldProgramParser {
+  
+  private static final Pattern DELIM = Pattern.compile("[,\\\\]");
+
+  public COGreeleyParser() {
+    super("Greeley", "CO",
+          "ID CALL ADDR! TEXT:INFO+ COMP:INFO+ PH:PHONE");
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+
+    body = body.replace('\n', ' ').replaceAll("  +", " ");
+    String[] flds = DELIM.split(body);
+    return super.parseFields(flds, data);
+  }
+  
+  private class MyIdField extends IdField {
+    public MyIdField() {
+      setPattern(Pattern.compile("\\d{5}"), true);
+    }
+  }
+  
+  private class MyAddressField extends AddressField {
+    
+    @Override
+    public void parse(String field, Data data) {
+      int pt = field.lastIndexOf('.');
+      if (pt < 0) abort();
+      data.strUnit = field.substring(pt+1).trim();
+      super.parse(field.substring(0,pt).trim(), data);
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return super.getFieldNames() + " UNIT";
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("ID")) return new MyIdField();
+    if (name.equals("ADDR")) return new MyAddressField();
+    return super.getField(name);
+  }
+}
