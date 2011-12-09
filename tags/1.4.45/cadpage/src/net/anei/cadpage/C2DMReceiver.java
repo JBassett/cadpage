935474d8b6762aa5c6079f263cba93e0dcc150d3
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2011-12-09 01:54:06 +0000

    Releae v1.4.45

---
+++ tags/1.4.45/cadpage/src/net/anei/cadpage/C2DMReceiver.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2007-2008 Esmertec AG.
+ * Copyright (C) 2007-2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.anei.cadpage;
+
+
+
+import net.anei.cadpage.parsers.SmsMsgParser;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Receives M2DM related intents 
+ */
+public class C2DMReceiver extends BroadcastReceiver {
+  
+  private static final String ACTION_C2DM_REGISTER = "com.google.android.c2dm.intent.REGISTER";
+  private static final String ACTION_C2DM_UNREGISTER = "com.google.android.c2dm.intent.UNREGISTER";
+  private static final String ACTION_C2DM_REGISTERED = "com.google.android.c2dm.intent.REGISTRATION";
+  private static final String ACTION_C2DM_RECEIVE = "com.google.android.c2dm.intent.RECEIVE";
+  private static final String C2DM_SENDER_EMAIL = "alerts@cadpage.org";
+  
+  private static Activity curActivity = null;
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    if (Log.DEBUG) Log.v("C2DMReceiver: onReceive()");
+    
+    // If initialization failure in progress, shut down without doing anything
+    if (TopExceptionHandler.isInitFailure()) return;
+    
+    if (ACTION_C2DM_REGISTERED.equals(intent.getAction())) {
+      handleRegistration(context, intent);
+    }
+    
+    else if (ACTION_C2DM_RECEIVE.equals(intent.getAction())) {
+      handleMessage(context, intent);
+    }
+  }
+  
+  private void handleRegistration(Context context, Intent intent) {
+    String error = intent.getStringExtra("error");
+    if (error != null) {
+      Log.w("C2DM registration failed: " + error);
+      if (curActivity != null) {
+        new AlertDialog.Builder(curActivity)
+              .setIcon(R.drawable.ic_launcher).setTitle("C2DM Registration Error")
+              .setPositiveButton(R.string.donate_btn_OK, null)
+              .setMessage(error)
+              .create().show();
+      }
+      return;
+    }
+    
+    String regId = intent.getStringExtra("unregistered");
+    if (regId != null) {
+      ManagePreferences.setRegistrationId(null);
+      Log.w("C2DM registration cancelled: " + regId);
+      if (curActivity != null) {
+        new AlertDialog.Builder(curActivity)
+              .setIcon(R.drawable.ic_launcher).setTitle("C2DM Registration Cancelled")
+              .setPositiveButton(R.string.donate_btn_OK, null)
+              .setMessage(regId)
+              .create().show();
+      }
+      return;
+    }
+    
+    regId = intent.getStringExtra("registration_id");
+    if (regId != null) {
+      ManagePreferences.setRegistrationId(regId);
+      Log.w("C2DM registration succeeded: " + regId);
+      if (curActivity != null) {
+        new AlertDialog.Builder(curActivity)
+              .setIcon(R.drawable.ic_launcher).setTitle("C2DM Registration Succeeded")
+              .setPositiveButton(R.string.donate_btn_OK, null)
+              .setMessage(regId)
+              .create().show();
+      }
+      return;
+    }
+    
+  }
+
+  private void handleMessage(Context context, Intent intent) {
+    
+    // If registration has been canceled, all C2DM messages should be ignored
+    if (ManagePreferences.registrationId() == null) return;
+    
+    // Reconstruct message from data
+    String from = intent.getStringExtra("from");
+    if (from == null) from = "C2DM";
+    String subject = intent.getStringExtra("subject");
+    if (subject == null) subject = "";
+    String content = intent.getStringExtra("content");
+    if (content == null) {
+      Log.w("C2DM message has no content");
+      return;
+    }
+    
+    SmsMmsMessage message = 
+      new SmsMmsMessage(from, subject, content, System.currentTimeMillis(), 
+                        SmsMmsMessage.MESSAGE_TYPE_C2DM);
+    
+    String location = intent.getStringExtra("format");
+    location = ManagePreferences.convertOldLocationCode(context, location);
+    message.setReqLocation(location);
+    message.setSponsor(intent.getStringExtra("sponsor"));
+    
+    // Add to log buffer
+    if (!SmsMsgLogBuffer.getInstance().add(message)) return;
+    
+    // See if the current parser will accept this as a CAD page
+    SmsMsgParser parser = ManageParsers.getInstance().getParser(null);
+    boolean isPage = parser.isPageMsg(message, true, false);
+    
+    // If it didn't, accept it as a general page.  We have to do something with
+    // it or it will just disappear
+    if (! isPage) {
+      SmsMsgParser genAlertParser = ManageParsers.getInstance().getAlertParser();
+      isPage = genAlertParser.isPageMsg(message, true, true);
+    }
+    
+    // This should never happen, but if the general alert parser can't handle
+    // it bail out
+    if (!isPage) return;
+    SmsReceiver.processCadPage(context, message);
+  }
+  
+  /**
+   * Register activity to be used to generate any alert dialogs
+   * @param activity
+   */
+  public static void registerActivity(Activity activity) {
+    curActivity = activity;
+  }
+  
+  /**
+   * Unregister activity previously registered for any generated alert dialogs
+   * @param activity
+   */
+  public static void unregisterActivity(Activity activity) {
+    if (curActivity == activity) curActivity = null;
+  }
+
+  public static void register(Context context) {
+    Intent intent = new Intent(ACTION_C2DM_REGISTER);
+    intent.putExtra("app", PendingIntent.getBroadcast(context, 0, new Intent(), 0));
+    intent.putExtra("sender", C2DM_SENDER_EMAIL);
+    context.startService(intent);
+  }
+  
+  public static void unregister(Context context) {
+    Intent intent = new Intent(ACTION_C2DM_UNREGISTER);
+    intent.putExtra("app", PendingIntent.getBroadcast(context, 0, new Intent(), 0));
+    context.startService(intent);
+  }
+
+  public static void emailRegistrationId(Context context) {
+    
+    // Build send email intent and launch it
+    Intent intent = new Intent(Intent.ACTION_SEND);
+    String emailSubject = CadPageApplication.getNameVersion() + " C2DM registrion ID";
+    intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+    intent.putExtra(Intent.EXTRA_TEXT, "My C2DM registration ID is " + ManagePreferences.registrationId());
+    intent.setType("message/rfc822");
+    context.startActivity(Intent.createChooser(
+        intent, context.getString(R.string.pref_sendemail_title)));
+  }
+}
+
---
+++ tags/1.4.45/cadpageTest/src/net/anei/cadpage/parsers/CT/CTTollandCountyParserTest.java
@@ -0,0 +1,123 @@
+package net.anei.cadpage.parsers.CT;
+
+import net.anei.cadpage.parsers.BaseParserTest;
+
+import org.junit.Test;
+
+
+public class CTTollandCountyParserTest extends BaseParserTest {
+  
+  public CTTollandCountyParserTest() {
+    setParser(new CTTollandCountyParser(), "TOLLAND COUNTY", "CT");
+  }
+  
+  @Test
+  public void testParser() {
+
+    doTest("T1",
+        "(TN Alert) 10 COTTAGE ST / UNIT D Vernon ALS Difficulty Breathing 21:14 Cross Street ST BERNARDS TER / SCHOOL ST 2011-00001770",
+        "ADDR:10 COTTAGE ST",
+        "APT:UNIT D",
+        "CITY:Vernon",
+        "CALL:ALS Difficulty Breathing",
+        "X:ST BERNARDS TER / SCHOOL ST",
+        "ID:2011-00001770");
+
+    doTest("T2",
+        "[TN Alert]  DOBSON RD / CAMPBELL AVE Vernon Vehicle Accident 2ND AMB  12:59 Cross Street 2011-00001773",
+        "ADDR:DOBSON RD & CAMPBELL AVE",
+        "CITY:Vernon",
+        "CALL:Vehicle Accident 2ND AMB",
+        "ID:2011-00001773");
+
+    doTest("T3",
+        "[TN Alert]  32 FRANKLIN PARK E / Vernon ALS DIFF BREATHING 09:07 Cross Street DEAD END / FRANKLIN ST 2011-00001772",
+        "ADDR:32 FRANKLIN PARK E",
+        "CITY:Vernon",
+        "CALL:ALS DIFF BREATHING",
+        "X:DEAD END / FRANKLIN ST",
+        "ID:2011-00001772");
+
+    doTest("T4",
+        "[TN Alert]  140 WINDSORVILLE RD / Vernon BLS back pain 02:21 Cross Street PENFIELD AVE / SEPTIC TREATMENT PLANT 2011-00001771",
+        "ADDR:140 WINDSORVILLE RD",
+        "CITY:Vernon",
+        "CALL:BLS back pain",
+        "X:PENFIELD AVE / SEPTIC TREATMENT PLANT",
+        "ID:2011-00001771");
+
+    doTest("T5",
+        "[TN Alert]  DOBSON RD / CAMPBELL AVE Vernon Vehicle Accident 12:59 Cross Street 2011-00001773",
+        "ADDR:DOBSON RD & CAMPBELL AVE",
+        "CITY:Vernon",
+        "CALL:Vehicle Accident",
+        "ID:2011-00001773");
+
+    doTest("T6",
+        "[TN Alert]  95 HARTFORD TPKE / Vernon * Golf Land Vehicle Accident MINOR INJURIES - EVALUATION",
+        "ADDR:95 HARTFORD TPKE",
+        "CITY:Vernon",
+        "CALL:Golf Land Vehicle Accident MINOR INJURIES - EVALUATION");
+
+    doTest("T7",
+        "[TN Alert]  67 BLUE RIDGE DR / Vernon Tree/Wires Down 14:38 Cross Street HICKORY HL / AUTUMN WOOD LN 2011-00001775",
+        "ADDR:67 BLUE RIDGE DR",
+        "CITY:Vernon",
+        "CALL:Tree/Wires Down",
+        "X:HICKORY HL / AUTUMN WOOD LN",
+        "ID:2011-00001775");
+
+    doTest("T8",
+        "(TN Alert) 155 W   MAIN ST 317 / Vernon ALS DIFFICULTY BREATHING 14:03 Cross Street ORCHARD ST, SPRING ST / WARD ST 2011-00001981",
+        "ADDR:155 W MAIN ST 317",
+        "CITY:Vernon",
+        "CALL:ALS DIFFICULTY BREATHING",
+        "X:ORCHARD ST, SPRING ST / WARD ST",
+        "ID:2011-00001981");
+
+    doTest("T9",
+        "(TN Alert) 36 GARDNER ST / Warehouse Point Fire Alarm 17:11 Cross Street PLEASANT ST / BRIDGE ST 2011-00000190",
+        "ADDR:36 GARDNER ST",
+        "CITY:Warehouse Point",
+        "CALL:Fire Alarm",
+        "X:PLEASANT ST / BRIDGE ST",
+        "ID:2011-00000190");
+
+    doTest("T10",
+        "(TN Alert) 69 BROOKLYN ST / (CLERKS OFC) Vernon BLS SICK- ELDERLY FEMALE  13:20 Cross Street COURT ST / VERNON AVE 2011-00002812",
+        "ADDR:69 BROOKLYN ST",
+        "APT:(CLERKS OFC)",
+        "CITY:Vernon",
+        "CALL:BLS SICK- ELDERLY FEMALE",
+        "X:COURT ST / VERNON AVE",
+        "ID:2011-00002812");
+
+    doTest("T11",
+        "(TN Alert) 29 BRACE RD / Somers ALS ARM INJURY, BLEEDING 22:42 Cross Street GEORGE WOOD RD / HALL HILL RD 2011-00000821",
+        "ADDR:29 BRACE RD",
+        "CITY:Somers",
+        "CALL:ALS ARM INJURY, BLEEDING",
+        "X:GEORGE WOOD RD / HALL HILL RD",
+        "ID:2011-00000821");
+
+    doTest("T12",
+        "(TN Alert) 69 COLTON RD / Somers ALS ? CVA 18:34 Cross Street PARSONS ST / SOKOL RD 2011-00000820",
+        "ADDR:69 COLTON RD",
+        "CITY:Somers",
+        "CALL:ALS ? CVA",
+        "X:PARSONS ST / SOKOL RD",
+        "ID:2011-00000820");
+
+    doTest("T13",
+        "(TN Alert) 234 JAMES RD / Ashford BLS Fall 15:25 Cross Street HORSE HILL RD / WESTFORD RD 2011-00001969",
+        "ADDR:234 JAMES RD",
+        "CITY:Ashford",
+        "CALL:BLS Fall",
+        "X:HORSE HILL RD / WESTFORD RD",
+        "ID:2011-00001969");
+  }
+  
+  public static void main(String[] args) {
+    new CTTollandCountyParserTest().generateTests("T1", "ADDR APT CITY CALL X ID");
+  }
+}
