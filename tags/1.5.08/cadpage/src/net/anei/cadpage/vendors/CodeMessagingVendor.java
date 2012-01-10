553017c005cd3dfc87b5b6c84969361d5dd37120
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-01-10 15:46:41 +0000

    v1.5.08

---
+++ tags/1.5.08/cadpage/src/net/anei/cadpage/vendors/CodeMessagingVendor.java
@@ -0,0 +1,16 @@
+package net.anei.cadpage.vendors;
+
+import net.anei.cadpage.R;
+
+class CodeMessagingVendor extends Vendor {
+  
+  CodeMessagingVendor() {
+    super(R.string.code_messaging_title,
+           R.string.code_messaging_summary,
+           R.string.code_messaging_text,
+           0,
+           0,
+           "http://www.CodePassport.net/CodePassport.asp?wci=Android",
+           ">CMG");
+  }
+}
