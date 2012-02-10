6c0cbec67aeec80608403b30e6821653e4ada54f
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-02-10 06:56:52 +0000

    v1.5.14

---
+++ tags/1.5.14/cadpage/src/net/anei/cadpage/parsers/NY/NYOrangeCountyParser.java
@@ -0,0 +1,16 @@
+package net.anei.cadpage.parsers.NY;
+
+import net.anei.cadpage.parsers.GroupBestParser;
+
+/*
+Orange County, NY
+ */
+
+
+public class NYOrangeCountyParser extends GroupBestParser {
+  
+  public NYOrangeCountyParser() {
+    super(new NYOrangeCountyAParser(),
+           new NYOrangeCountyBParser());
+  }
+}
