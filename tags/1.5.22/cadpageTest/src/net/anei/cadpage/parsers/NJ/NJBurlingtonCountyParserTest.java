b0cfc549fb68f3b35c0646da0baca0fe7ac7b665
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-03-16 06:47:03 +0000

    v1.5.22

---
+++ tags/1.5.22/cadpageTest/src/net/anei/cadpage/parsers/NJ/NJBurlingtonCountyParserTest.java
@@ -0,0 +1,27 @@
+package net.anei.cadpage.parsers.NJ;
+
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Enclosed.class)
+public class NJBurlingtonCountyParserTest {
+  
+  public static class TestA extends NJBurlingtonCountyAParserTest {
+    public TestA() {
+      setParser(new NJBurlingtonCountyParser());
+    }
+  }
+  
+  public static class TestB extends NJBurlingtonCountyBParserTest {
+    public TestB() {
+      setParser(new NJBurlingtonCountyParser());
+    }
+  }
+  
+  public static class TestC extends NJBurlingtonCountyCParserTest {
+    public TestC() {
+      setParser(new NJBurlingtonCountyParser());
+    }
+  }
+}
