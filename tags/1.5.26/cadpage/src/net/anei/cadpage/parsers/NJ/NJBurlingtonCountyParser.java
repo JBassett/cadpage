bf4183c1c9f2ee73d5a193d2e449dd463f726a9d
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2012-04-08 02:22:39 +0000

    v1.5.26

---
+++ tags/1.5.26/cadpage/src/net/anei/cadpage/parsers/NJ/NJBurlingtonCountyParser.java
@@ -0,0 +1,17 @@
+package net.anei.cadpage.parsers.NJ;
+
+import net.anei.cadpage.parsers.GroupBestParser;
+
+
+/*
+Burlington County, NJ
+*/
+
+public class NJBurlingtonCountyParser extends GroupBestParser {
+  
+  public NJBurlingtonCountyParser() {
+    super(new NJBurlingtonCountyAParser(), 
+          new NJBurlingtonCountyBParser(),
+          new NJBurlingtonCountyCParser());
+  }
+}
