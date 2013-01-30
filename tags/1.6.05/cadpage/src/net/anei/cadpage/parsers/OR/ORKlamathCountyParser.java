4c73b780446fb3c15d6b9869895573e7894fc8d7
kencorbin101 <kencorbin101@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-01-30 18:50:13 +0000

    v1.6.05

---
+++ tags/1.6.05/cadpage/src/net/anei/cadpage/parsers/OR/ORKlamathCountyParser.java
@@ -0,0 +1,72 @@
+package net.anei.cadpage.parsers.OR;
+
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+import net.anei.cadpage.parsers.FieldProgramParser;
+import net.anei.cadpage.parsers.MsgInfo.Data;
+
+
+
+public class ORKlamathCountyParser extends FieldProgramParser {
+  
+  private static Pattern KEYWORD_PTN = Pattern.compile("(?<=\n(?:EVENT #|PRIORITY|LOCATION|CITY|APT|PREMISE|COMMENT))");
+
+  private static Properties CITY_CODES = buildCodeTable(new String[]{
+      "KF", "KLAMATH FALLS"
+  });
+  
+  public ORKlamathCountyParser() {
+    super(CITY_CODES, "KLAMATH COUNTY", "OR",
+           "DATETIME! EVENT_#:IDUNIT! CALL! PRIORITY:PRI! LOCATION:ADDR! CITY:CITY! APT:APT! PREMISE:PLACE? COMMENT:INFO+");
+  }
+  
+  @Override
+  public String getFilter() {
+    return "kc-911@kc911.us";
+  }
+
+  @Override
+  protected boolean parseMsg(String body, Data data) {
+    body = KEYWORD_PTN.matcher(body).replaceAll(":");
+    return parseFields(body.split("\n"), data);
+  }
+  
+  private static final Pattern DATE_TIME_PTN = Pattern.compile("(\\d{1,2}/\\d{1,2}/\\d{4}) (\\d{4})");
+  private class MyDateTimeField extends DateTimeField {
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = DATE_TIME_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strDate = match.group(1);
+      String time = match.group(2);
+      data.strTime = time.substring(0,2) + ':' + time.substring(2,4);
+    }
+  }
+  
+  private static final Pattern ID_UNIT_PTN = Pattern.compile("(\\d{10}) (.*)");
+  private class MyIdUnitField extends Field {
+    
+    @Override
+    public void parse(String field, Data data) {
+      Matcher match = ID_UNIT_PTN.matcher(field);
+      if (!match.matches()) abort();
+      data.strCallId = match.group(1).trim();
+      data.strUnit =  match.group(2).trim();
+    }
+    
+    @Override
+    public String getFieldNames() {
+      return "ID UNIT";
+    }
+  }
+  
+  @Override
+  public Field getField(String name) {
+    if (name.equals("DATETIME")) return new MyDateTimeField();
+    if (name.equals("IDUNIT")) return new MyIdUnitField();
+    return super.getField(name);
+  }
+}
---
+++ tags/1.6.05/cadpageTest/rot13-src/PatternTest.java
@@ -0,0 +1,28 @@
+
+
+vzcbeg wnin.hgvy.ertrk.Zngpure;
+vzcbeg wnin.hgvy.ertrk.Cnggrea;
+
+choyvp pynff CnggreaGrfg {
+
+  cevingr fgngvp svany Cnggrea ZNFGRE 
+      = Cnggrea.pbzcvyr("Yn = (\\q+)Âº ([\\q,]+)'([AF])");
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    qbGrfg("Yn = 59Âº 23.486'A    Yb = 016Âº 28.492'R");
+  }
+    
+  
+  cevingr fgngvp ibvq qbGrfg(Fgevat grfg) {
+    Zngpure zngpu = ZNFGRE.zngpure(grfg);
+    Flfgrz.bhg.cevag(grfg);
+    vs (! zngpu.svaq()) {
+      Flfgrz.bhg.cevag(" ab zngpu");
+    } ryfr {
+      sbe (vag t = 0; t<=zngpu.tebhcPbhag(); t++) {
+        Flfgrz.bhg.cevag(" T" + t + ":" + zngpu.tebhc(t));
+      }
+    }
+    Flfgrz.bhg.cevagya();
+  }
+
+}
