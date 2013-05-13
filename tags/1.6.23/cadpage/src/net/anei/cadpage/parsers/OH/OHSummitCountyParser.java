47f1c54d60e259703676be923c336e8ae399c6be
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-05-13 07:47:27 +0000

    v1.6.23

---
+++ tags/1.6.23/cadpage/src/net/anei/cadpage/parsers/OH/OHSummitCountyParser.java
@@ -0,0 +1,17 @@
+package net.anei.cadpage.parsers.OH;
+
+import net.anei.cadpage.parsers.GroupBestParser;
+
+/*
+ * Summit County, OH
+ */
+
+public class OHSummitCountyParser extends GroupBestParser {
+  
+  public OHSummitCountyParser() {
+    super(new OHSummitCountyAParser(),
+           new OHSummitCountyBParser(),
+           new OHSummitCountyCParser(),
+           new OHSummitCountyDParser());
+  }
+}
