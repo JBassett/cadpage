1e773b8f2f6ce564781abbfa8a75372b9acba0ff
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2013-12-23 16:20:38 +0000

    v1.6.59

---
+++ tags/1.6.59/cadpage/src/net/anei/cadpage/parsers/CT/CTLitchfieldCountyParser.java
@@ -0,0 +1,53 @@
+package net.anei.cadpage.parsers.CT;
+
+import net.anei.cadpage.parsers.GroupBestParser;
+
+
+public class CTLitchfieldCountyParser extends GroupBestParser {
+  
+  public CTLitchfieldCountyParser() {
+    super(new CTLitchfieldCountyAParser(), new CTLitchfieldCountyBParser());
+  }
+  
+  static String[] CITY_LIST = new String[]{
+    "BARKHAMSTED",
+    "BETHLEHEM",
+    "BRIDGEWATER",
+    "CANAAN",
+    "COLEBROOK",
+    "CORNWALL",
+    "GOSHEN",
+    "HARWINTON",
+    "NORTHWEST HARWINTON",
+    "KENT",
+    "SOUTH KENT",
+    "LITCHFIELD",
+    "BANTAM",
+    "MORRIS",
+    "NEW HARTFORD",
+    "NEW MILFORD",
+    "GAYLORDSVILLE",
+    "NORFOLK",
+    "NORTH CANAAN",
+    "PLYMOUTH",
+    "TERRYVILLE",
+    "ROXBURY",
+    "SALISBURY",
+    "SHARON",
+    "THOMASTON",
+    "TORRINGTON",
+    "WARREN",
+    "WASHINGTON",
+    "NEW PRESTON",
+    "WATERTOWN",
+    "OAKVILLE",
+    "WINCHESTER",
+    "WINSTED",
+    "WOODBURY",
+
+    "CANTON",
+    "HARTFORD",
+    "SHERMAN",
+    "ROXBURY"
+  };
+}
---
+++ tags/1.6.59/cadpageTest/rot13-src/net/anei/cadpage/parsers/PA/PAArmstrongCountyParserTest.java
@@ -0,0 +1,268 @@
+cnpxntr arg.narv.pnqcntr.cnefref.CN;
+
+vzcbeg arg.narv.pnqcntr.cnefref.OnfrCnefreGrfg;
+
+vzcbeg bet.whavg.Grfg;
+
+/*
+Nezfgebat Pbhagl, CN
+Pbagnpg: yneel hzonhtu <yhzonhtu17@tznvy.pbz>
+Fraqre: 911qvfcngpu@pb.nezfgebat.cn.hf
+
+473 GEBL UVYY EQ NVECBEG EQ / ZPTERTBE EQ SZVFP 39992 05:40 7245453398
+11878 FGNGR EBHGR 85 NEZFQNYR EQ / PELGMRE EQ BECUNAF BS GUR FGBEZ INRAG 40313 19:09 7247120225
+141 NQNZF EQ FGNGR EBHGR 85 / ONYYBPX YA SZVFP 40720 09:07 7247837418
+[Qvfcngpu]  580 A PUREEL FG UBBXF YA / FCEHPR YA SFGE2 41628 13:58 7246640045
+[Qvfcngpu]  868 FGNGR EBHGR 28/66 ZRPUYVAT EQ / FYBNA UVYY EQ INRAG 42068 15:55 7248597657
+
+Pbagnpg: oenaqba xvytber <sverstugre10@tznvy.pbz>
+Fraqre: 911Qvfcngpu@pb.nezfgebat.cn.hf
+(Qvfcngpu) SERRCBEG EQ, HF EBHGR 422 / CBAL SNEZ EQ, HF EBHGR 422 INRAG 3706 06:42
+(Qvfcngpu) 13 UVYYGBC CYNMN PBZZREPR QE / SENAXYVA UVYY EQ UBYVQNL VAASNSNYE 5238 15:39 8887467539
+(Qvfcngpu) 592 GNEEGBJA EQ SHEANPR EHA EQ / FGNGR EBHGR 1038, OHGYRE EQ CW TERPBSIRU 5258 17:45 7249548780
+
+Pbagnpg: Wnpbo Qviryl <wcqviryl@tznvy.pbz>
+Fraqre: 911Qvfcngpu@pb.nezfgebat.cn.hf
+(Qvfcngpu) 105 RYZ QE ZNCYR YA / YBPHFG YA SPBNYZ 4524 14:11 8003643616
+(Qvfcngpu) 571 RNFG OENQL EQ EBYYVAT UVYYF EQ / PBEAZNA QE INVAW 4360 01:41 7245451228
+
+Pbagnpg: Rina Xrrare <xrrare530@ubgznvy.pbz>
+Fraqre: 911qvfcngpu@pb.nezfgebat.cn.hf
+209 F CBCYNE FG FCEHPR NIR / J FBHGU NIR RSNYY1 38534 03:52 8142578971
+241 CVCRYVAR EQ ZNCYR YRNS EQ / QNLGBA EQ ZPZRNAF OHF PBZCNAL RFVPX1 38440 09:09 8142578245
+311 EVQTR EQ JBBQF EQ/ PERRX EQ SZIN 33541 02:43 7244484701
+(Qvfcngpu) 217 R ZNVA FG A ERORPPN FG / F FPUBBY FG, A FPUBBY FG RSNYY1 38682 13:28 8142578148
+
+Pbagnpg: Wnxr Znegva Nppg <znegva13975@tznvy.pbz>
+Fraqre: 911Qvfcngpu@pb.nezfgebat.cn.hf
+(Qvfcngpu) 1346 FGNGR EBHGR 4007 OREANEQ EQ / FGNGR EBHGR 268 19898 11:02 7243093328 SFGE1
+
+Pbagnpg: Rnfg Senaxyva & Nccyrjbyq ISQ <rnfgsenaxyvasver@lnubb.pbz>
+Fraqre: 911Qvfcngpu@pb.nezfgebat.cn.hf
+(Qvfcngpu) 610 RNFG OENQL EQ EBYYVAT UVYYF EQ / OBYGM EQ 19932 17:03 7248598547 INRAG
+
+Pbagnpg: oenaqba xvytber <o53xym@tznvy.pbz>
+Fraqre: 911Qvfcngpu@pb.nezfgebat.cn.hf
+(Qvfcngpu) GRFG NHGBZNGVP SVER NYNEZ  RNFG SENAXYVA SVER QRCNEGZRAG ZNVA 165 RNFG OENQL EQ SVERUBHFR YA / FUREJBBQ QE 19974 23:17
+(Qvfcngpu) R1 PURFG QVFPBZSBEG  128 R  ENVYEBNQ NIR A FPUBBY FG / CRAA NIR 20067 16:35
+
+*/
+
+choyvp pynff CNNezfgebatPbhaglCnefreGrfg rkgraqf OnfrCnefreGrfg {
+  
+  choyvp CNNezfgebatPbhaglCnefreGrfg() {
+    frgCnefre(arj CNNezfgebatPbhaglCnefre(), "NEZFGEBAT PBHAGL", "CN");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre() {
+
+    qbGrfg("G1",
+        "473 GEBL UVYY EQ NVECBEG EQ / ZPTERTBE EQ SZVFP 39992 05:40 7245453398",
+        "NQQE:473 GEBL UVYY EQ",
+        "K:NVECBEG EQ / ZPTERTBE EQ",
+        "PNYY:SZVFP",
+        "VQ:39992",
+        "GVZR:05:40",
+        "CUBAR:7245453398");
+
+    qbGrfg("G2",
+        "11878 FGNGR EBHGR 85 NEZFQNYR EQ / PELGMRE EQ BECUNAF BS GUR FGBEZ INRAG 40313 19:09 7247120225",
+        "NQQE:11878 FGNGR EBHGR 85",
+        "ZNQQE:11878 FGNGR 85",
+        "K:NEZFQNYR EQ / PELGMRE EQ",
+        "CYNPR:BECUNAF BS GUR FGBEZ",
+        "PNYY:INRAG",
+        "VQ:40313",
+        "GVZR:19:09",
+        "CUBAR:7247120225");
+
+    qbGrfg("G3",
+        "141 NQNZF EQ FGNGR EBHGR 85 / ONYYBPX YA SZVFP 40720 09:07 7247837418",
+        "NQQE:141 NQNZF EQ",
+        "K:FGNGR EBHGR 85 / ONYYBPX YA",
+        "PNYY:SZVFP",
+        "VQ:40720",
+        "GVZR:09:07",
+        "CUBAR:7247837418");
+
+    qbGrfg("G4",
+        "[Qvfcngpu]  580 A PUREEL FG UBBXF YA / FCEHPR YA SFGE2 41628 13:58 7246640045",
+        "NQQE:580 A PUREEL FG",
+        "K:UBBXF YA / FCEHPR YA",
+        "PNYY:SFGE2",
+        "VQ:41628",
+        "GVZR:13:58",
+        "CUBAR:7246640045");
+
+    qbGrfg("G5",
+        "[Qvfcngpu]  868 FGNGR EBHGR 28/66 ZRPUYVAT EQ / FYBNA UVYY EQ INRAG 42068 15:55 7248597657",
+        "NQQE:868 FGNGR EBHGR 28",
+        "ZNQQE:868 FGNGR 28",
+        "K:66 ZRPUYVAT EQ / FYBNA UVYY EQ",
+        "PNYY:INRAG",
+        "VQ:42068",
+        "GVZR:15:55",
+        "CUBAR:7248597657");
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefreOenaqba() {
+
+//    qbGrfg("G1",
+//        "(Qvfcngpu) SERRCBEG EQ, HF EBHGR 422 / CBAL SNEZ EQ, HF EBHGR 422 INRAG 3706 06:42",
+//        "NQQE:SERRCBEG EQ & HF EBHGR 422",
+//        "ZNQQE:SERRCBEG EQ & HF 422",
+//        "K:CBAL SNEZ EQ / HF EBHGR 422",
+//        "PNYY:INRAG",
+//        "VQ:3706",
+//        "GVZR:06:42");
+
+    qbGrfg("G2",
+        "(Qvfcngpu) 13 UVYYGBC CYNMN PBZZREPR QE / SENAXYVA UVYY EQ UBYVQNL VAASNSNYE 5238 15:39 8887467539",
+        "NQQE:13 UVYYGBC CYNMN",
+        "K:PBZZREPR QE / SENAXYVA UVYY EQ",
+        "CYNPR:UBYVQNL",
+        "PNYY:VAASNSNYE",
+        "VQ:5238",
+        "GVZR:15:39",
+        "CUBAR:8887467539");
+
+    qbGrfg("G3",
+        "(Qvfcngpu) 592 GNEEGBJA EQ SHEANPR EHA EQ / FGNGR EBHGR 1038, OHGYRE EQ CW TERPBSIRU 5258 17:45 7249548780",
+        "NQQE:592 GNEEGBJA EQ",
+        "K:SHEANPR EHA EQ / FGNGR EBHGR 1038 / OHGYRE EQ",
+        "CYNPR:CW",
+        "PNYY:TERPBSIRU",
+        "VQ:5258",
+        "GVZR:17:45",
+        "CUBAR:7249548780");
+
+  }
+  
+  @Grfg
+  choyvp ibvq grfgCnefre3() {
+
+    qbGrfg("G1",
+        "(Qvfcngpu) 105 RYZ QE ZNCYR YA / YBPHFG YA SPBNYZ 4524 14:11 8003643616",
+        "NQQE:105 RYZ QE",
+        "K:ZNCYR YA / YBPHFG YA",
+        "PNYY:SPBNYZ",
+        "VQ:4524",
+        "GVZR:14:11",
+        "CUBAR:8003643616");
+
+    qbGrfg("G2",
+        "(Qvfcngpu) 571 RNFG OENQL EQ EBYYVAT UVYYF EQ / PBEAZNA QE INVAW 4360 01:41 7245451228",
+        "NQQE:571 RNFG OENQL EQ",
+        "K:EBYYVAT UVYYF EQ / PBEAZNA QE",
+        "PNYY:INVAW",
+        "VQ:4360",
+        "GVZR:01:41",
+        "CUBAR:7245451228");
+
+  }
+  
+  @Grfg
+  choyvp ibvq grfgRinaXrrare() {
+
+    qbGrfg("G1",
+        "209 F CBCYNE FG FCEHPR NIR / J FBHGU NIR RSNYY1 38534 03:52 8142578971",
+        "NQQE:209 F CBCYNE FG",
+        "K:FCEHPR NIR / J FBHGU NIR",
+        "PNYY:RSNYY1",
+        "VQ:38534",
+        "GVZR:03:52",
+        "CUBAR:8142578971");
+
+    qbGrfg("G2",
+        "241 CVCRYVAR EQ ZNCYR YRNS EQ / QNLGBA EQ ZPZRNAF OHF PBZCNAL RFVPX1 38440 09:09 8142578245",
+        "NQQE:241 CVCRYVAR EQ",
+        "K:ZNCYR YRNS EQ / QNLGBA EQ",
+        "CYNPR:ZPZRNAF OHF PBZCNAL",
+        "PNYY:RFVPX1",
+        "VQ:38440",
+        "GVZR:09:09",
+        "CUBAR:8142578245");
+
+    qbGrfg("G3",
+        "311 EVQTR EQ JBBQF EQ/ PERRX EQ SZIN 33541 02:43 7244484701",
+        "NQQE:311 EVQTR EQ",
+        "K:JBBQF EQ / PERRX EQ",
+        "PNYY:SZIN",
+        "VQ:33541",
+        "GVZR:02:43",
+        "CUBAR:7244484701");
+
+    qbGrfg("G4",
+        "(Qvfcngpu) 217 R ZNVA FG A ERORPPN FG / F FPUBBY FG, A FPUBBY FG RSNYY1 38682 13:28 8142578148",
+        "NQQE:217 R ZNVA FG A",
+        "K:ERORPPN FG / F FPUBBY FG / A FPUBBY FG",
+        "PNYY:RSNYY1",
+        "VQ:38682",
+        "GVZR:13:28",
+        "CUBAR:8142578148");
+
+  }
+  
+  @Grfg
+  choyvp ibvq grfgWnxrZnegva() {
+
+    qbGrfg("G1",
+        "(Qvfcngpu) 1346 FGNGR EBHGR 4007 OREANEQ EQ / FGNGR EBHGR 268 19898 11:02 7243093328 SFGE1",
+        "NQQE:1346 FGNGR EBHGR 4007",
+        "ZNQQE:1346 FGNGR 4007",
+        "K:OREANEQ EQ / FGNGR EBHGR 268",
+        "VQ:19898",
+        "GVZR:11:02",
+        "CUBAR:7243093328",
+        "PNYY:SFGE1");
+  
+  }
+  
+  @Grfg
+  choyvp ibvq grfgRnfgSenaxyvaNaqNccyrjbq() {
+
+    qbGrfg("G1",
+        "(Qvfcngpu) 610 RNFG OENQL EQ EBYYVAT UVYYF EQ / OBYGM EQ 19932 17:03 7248598547 INRAG",
+        "NQQE:610 RNFG OENQL EQ",
+        "K:EBYYVAT UVYYF EQ / OBYGM EQ",
+        "VQ:19932",
+        "GVZR:17:03",
+        "CUBAR:7248598547",
+        "PNYY:INRAG");
+   
+  }
+  
+  @Grfg
+  choyvp ibvq grfgOenaqbaXvytber2() {
+
+    qbGrfg("G1",
+        "(Qvfcngpu) GRFG NHGBZNGVP SVER NYNEZ  RNFG SENAXYVA SVER QRCNEGZRAG ZNVA 165 RNFG OENQL EQ SVERUBHFR YA / FUREJBBQ QE 19974 23:17",
+        "PNYY:GRFG NHGBZNGVP SVER NYNEZ",
+        "CYNPR:RNFG SENAXYVA SVER QRCNEGZRAG ZNVA",
+        "NQQE:165 RNFG OENQL EQ",
+        "K:SVERUBHFR YA / FUREJBBQ QE",
+        "VQ:19974",
+        "GVZR:23:17");
+    
+  }
+  
+  @Grfg
+  choyvp ibvq grfgRinaXrrare2() {
+
+    qbGrfg("G1",
+        "(Qvfcngpu) R1 PURFG QVFPBZSBEG  128 R  ENVYEBNQ NIR A FPUBBY FG / CRAA NIR 20067 16:35",
+        "PNYY:R1 PURFG QVFPBZSBEG",
+        "NQQE:128 R ENVYEBNQ NIR",
+        "K:A FPUBBY FG / CRAA NIR",
+        "VQ:20067",
+        "GVZR:16:35");
+
+  }
+  
+  
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    arj CNNezfgebatPbhaglCnefreGrfg().trarengrGrfgf("G1");
+  }
+}
