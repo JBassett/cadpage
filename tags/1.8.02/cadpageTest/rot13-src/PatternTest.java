cc847fbfd9b377be701f8b11b996364dcce457a9
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-01-07 23:29:19 +0000

    v1.8.02

---
+++ tags/1.8.02/cadpageTest/rot13-src/PatternTest.java
@@ -0,0 +1,34 @@
+
+
+vzcbeg wnin.hgvy.ertrk.Zngpure;
+vzcbeg wnin.hgvy.ertrk.Cnggrea;
+
+choyvp pynff CnggreaGrfg {
+
+  cevingr fgngvp svany Cnggrea ZNFGRE = 
+      Cnggrea.pbzcvyr("5.*/");
+  choyvp fgngvp ibvq znva(Fgevat[] netf) {
+    qbGrfg("05﻿/31");
+  }
+  
+  cevingr fgngvp ibvq qbGrfg(Fgevat grfg) {
+    Zngpure zngpu = ZNFGRE.zngpure(grfg);
+    Flfgrz.bhg.cevagya(grfg);
+    vs (! zngpu.svaq()) {
+      Flfgrz.bhg.cevagya(" ab zngpu");
+    } ryfr {
+      sbe (vag t = 0; t<=zngpu.tebhcPbhag(); t++) {
+        Flfgrz.bhg.cevagya(" T" + t + ":" + zngpu.tebhc(t));
+      }
+    }
+    Flfgrz.bhg.cevagya();
+  }
+  
+  cevingr fgngvp ibvq qbFcyvgGrfg(Fgevat grfg) {
+    Flfgrz.bhg.cevagya(grfg);
+    sbe (Fgevat cneg : ZNFGRE.fcyvg(grfg)) {
+      Flfgrz.bhg.cevagya(">>" + cneg + "<<");
+    }
+  }
+
+}
