045d95e13561655b089704cdb44a0133cdcdf5f1
kencorbin101@gmail.com <kencorbin101@gmail.com@ed8909fc-68fd-3c11-af7c-6c41d973aed0>
2015-01-14 19:27:31 +0000

    v1.8.03

---
+++ tags/1.8.80/cadpage/src/net/anei/cadpage/parsers/CT/CTStamfordParser.java
@@ -0,0 +1,21 @@
+package net.anei.cadpage.parsers.CT;
+
+import java.util.regex.Pattern;
+
+import net.anei.cadpage.parsers.dispatch.DispatchA3Parser;
+
+/**
+ * Stamford, CT
+ */
+public class CTStamfordParser extends DispatchA3Parser {
+  
+  private static final Pattern PREFIX_PTN = Pattern.compile("^(?:911Fire:)?\\*\\n");
+  public CTStamfordParser() {
+    super(0, PREFIX_PTN, "STAMFORD", "CT");
+  }
+
+  @Override
+  public String getFilter() {
+    return "911fire@ci.Stamford.ct.us";
+  }
+}
